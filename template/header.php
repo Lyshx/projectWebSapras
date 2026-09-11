3bea290a22d571945eda87aa5d2f664e753b3430
Lyshx <jovankasherly@gmail.com>
2026-09-11 10:04:48 +0700

    update beberapa file

---
+++ template/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?= $title ?></title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="<?= $main_url ?>asset/sb_admin/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <link rel="shortcut icon" href="<?= $main_url ?>asset/img/asset/ChatGPT_Image_11_Sep_2026__09.45.22-removebg-preview.png" type="image/x-icon">
+    </head>
