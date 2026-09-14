c385a0b1117ab4ce2fa5e43428e94d39ae65ede7
nurulbadri4693-bit <nurulbadri4693@gmail.com>
2026-09-14 14:01:56 +0700

    mengubah tambah.php

--- tambah.php
+++ tambah.php
@@ -10,6 +10,7 @@ if(isset($_POST['submit'])) {
     $query = "INSERT INTO barang (id_barang, nama_barang, jumlah, kondisi, stok_barang, lokasi) VALUES ('$id_barang', '$nama_barang', '$jumlah', '$kondisi', '$stok_barang', '$lokasi')";
     mysqli_query($koneksi, $query);
     header("Location: tampil.php");
+    
 }
 ?>
 
