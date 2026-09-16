97b38a2f74f3ac6642a8c291035b23097773ab5e
nurulbadri4693-bit <nurulbadri4693@gmail.com>
2026-09-16 10:12:16 +0700

    tampil_peminjam.php

--- tampil_peminjam.php
+++ tampil_peminjam.php
@@ -1,6 +1,6 @@
 <?php
 include "koneksi.php";
-$query = "SELECT * FROM pemijam";
+$query = "SELECT * FROM peminjam";
 $hasil = mysqli_query($koneksi, $query);
 $data = mysqli_fetch_all($hasil, MYSQLI_ASSOC);
 ?>
@@ -10,7 +10,7 @@ $data = mysqli_fetch_all($hasil, MYSQLI_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabel Barang</title>
+    <title>Tabel Peminjam</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
