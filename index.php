3662932714766d13c8e1896973ed155c58bd5aa2
fazzzzzril45 <muhamadfahazril05@gmail.com>
2026-09-16 08:35:17 +0700

    update file

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">        
-    <body class="sb-nav-fixed">
+<?php
+
+require_once  "config.php";
+
+$title = "Dashboard - SMKN 1 MAJA";
+require_once "template/header.php";
+require_once "template/navbar.php";
+require_once "template/sidebar.php";
+
+?>
+
+   <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
             <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
@@ -651,26 +660,10 @@
                         </div>
                     </div>
                 </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="js/scripts.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="js/datatables-simple-demo.js"></script>
-    </body>
-</html>
+
+<?php
+
+    require_once "template/footer.php";
+
+?>
+
