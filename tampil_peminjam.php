4e3602939ce7d7c954d33a17aa7c1c48546a946b
nurulbadri4693-bit <nurulbadri4693@gmail.com>
2026-09-16 10:09:48 +0700

    tampil_peminjam.php

--- tampil_peminjam.php
+++ tampil_peminjam.php
@@ -1,6 +1,6 @@
 <?php
 include "koneksi.php";
-$query = "SELECT * FROM barang";
+$query = "SELECT * FROM pemijam";
 $hasil = mysqli_query($koneksi, $query);
 $data = mysqli_fetch_all($hasil, MYSQLI_ASSOC);
 ?>
