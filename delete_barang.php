312a725ecf7e94300aff9c7ba8bcbf407cfd8615
nurulbadri4693-bit <nurulbadri4693@gmail.com>
2026-09-16 09:30:23 +0700

    delete_barang.php

--- delete_barang.php
+++ delete_barang.php
@@ -1,8 +1,8 @@
 <?php
 include "koneksi.php";
 $id = $_GET ['id'];
-$query = "DELETE FORM sapras WHERE id='$id";
-mysqli_query ($koneksi,query);
+$query = "DELETE FROM barang WHERE id='$id'";
+mysqli_query ($koneksi, $query);
 header ("location : tampil.php");
 exit;
 ?>
